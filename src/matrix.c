cddc328ec2a99bdcd06b4b0af0d640d815401ff8
Rafael Terra <rafaelstjf@hotmail.com>
2020-03-12 08:39:58 -0300

    matrix.c, makefile e arquivo de entrada criados

---
+++ src/matrix.c
@@ -0,0 +1,33 @@
+#include "matrix.h"
+int matrix_create(Matrix **m){
+
+ return 0;   
+}
+int matrix_destroy(Matrix *m){
+
+    return 0;
+}
+int matrix_print(const Matrix *m){
+    
+    return 0;
+}
+int matrix_add(const Matrix *m, const Matrix *n, Matrix **r){
+
+    return 0;
+}
+int matrix_multiply(const Matrix *m, const Matrix *n, Matrix **r){
+    
+    return 0;
+}
+int matrix_transpose(const Matrix *m, Matrix **r){
+
+    return 0;
+}
+int matrix_getelem(const Matrix *m, int x, int y, float *elem){
+    
+    return 0;
+}
+int matrix_setelem(Matrix *m, int x, int y, float elem){
+
+    return 0;
+}
